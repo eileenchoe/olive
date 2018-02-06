38a275f9b28016cf3f0d9059c5fddaf141f17dc3
Eileen Choe <echoe@lion.lmu.edu>
2018-02-06 15:29:22 -0800

    added support for incorrect syntax files

--- test/parser/parser-test.js
+++ test/parser/parser-test.js
@@ -11,7 +11,18 @@ const parse = require('../../syntax/parser');
 
 describe('The parser', () => {
   fs.readdirSync(__dirname).forEach((name) => {
-    if (name.endsWith('.oil')) {
+    if (name.startsWith('incorrect')) {
+      it(`throws for incorrect syntax for ${name}`, (done) => {
+        fs.readFile(`${__dirname}/${name}`, 'utf-8', (err, input) => {
+          expect(() => { parse(input); }).toThrow();
+          done();
+          // fs.readFile(`${__dirname}/${name}.json`, 'utf-8', (_err, expected) => {
+          //   assert.deepEqual(ast, JSON.parse(expected));
+          //   done();
+          // });
+        });
+      });
+    } else if (name.endsWith('.oil')) {
       it(`produces the correct AST for ${name}`, (done) => {
         fs.readFile(`${__dirname}/${name}`, 'utf-8', (err, input) => {
           const ast = parse(input);
